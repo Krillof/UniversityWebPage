b90750e3e12325404f03f85e0c4778194f584376
DirAlmeri <kz07082003@gmail.com>
2022-12-25 14:17:35 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,19 @@ export default {
   }
 }
 </script>
+<!-- 
+} -->
 
 <style>
+@font-face {
+  font-family: 'Montserrat';
+  font-display: auto;
+  src: local('Montserrat Regular'), local('Montserrat-Regular'), url('https://fonts.googleapis.com/css?family=Montserrat&display=swap') format('woff2'), url('http://hb.bizmrg.com/fonts/montserrat/400.woff') format('woff');
+  font-weight: 50;
+  font-style: normal;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
