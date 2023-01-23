3877bf39e07e76a06da51029215c1b1fa0b134e0
Dill <dima.liksukov@mail.ru>
2023-01-23 10:59:35 +0300

    last

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: '',
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/UniversityWebPage-main/'
+    : '/'
+}
